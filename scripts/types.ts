14489a65e40c739e6539c3c7e0ff32f263113319
Lucas Pollice <lucasp465@gmail.com>
2023-02-04 02:45:56 -0500

    simplify types

--- scripts/types.ts
+++ scripts/types.ts
@@ -10,18 +10,8 @@ type OptionsDict = { [key: string]: Options }
 /**
  * The original CommandModule type did not make the `command` field consistent with `handler` so here is a new type.
  */
-type MyCommandModule<O extends OptionsDict> = {
-  /** array of strings (or a single string) representing aliases of `exports.command`, positional args defined in an alias are ignored */
-  aliases?: ReadonlyArray<string> | string | undefined
-  /** object declaring the options the command accepts, or a function accepting and returning a yargs instance */
+interface MyCommandModule<O extends OptionsDict> extends CommandModule<unknown, InferredOptionTypes<O>> {
   builder?: O & OptionsDict
-  /** string (or array of strings) that executes this command when given on the command line, first string may contain positional args */
-  command?: ReadonlyArray<string> | string | undefined
-  /** boolean (or string) to show deprecation notice */
-  deprecated?: boolean | string | undefined
-  /** string used as the description for the command in help text, use `false` for a hidden command */
-  describe?: string | false | undefined
-  /** a function which will be passed the parsed argv. */
   handler: (
     args: ArgumentsCamelCase<InferredOptionTypes<O>>
   ) => void | Promise<void>
@@ -30,8 +20,6 @@ type MyCommandModule<O extends OptionsDict> = {
 export const asCommand = <T extends OptionsDict>(
   module: MyCommandModule<T>
 ): CommandModule<
-  {
-    /*_*/
-  },
+  unknown,
   InferredOptionTypes<T>
 > => module
