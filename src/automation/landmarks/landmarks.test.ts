dbbd827fca9ea64054000bbdc28b4cae8132f509
Lucas Pollice <lp1935@nyu.edu>
2023-02-07 20:55:01 -0500

    remove log

--- src/automation/landmarks/landmarks.test.ts
+++ src/automation/landmarks/landmarks.test.ts
@@ -1,10 +1,8 @@
 import { load } from './landmarks'
-import { ScreenMap } from './types'
 
 describe('landmarks', () => {
   it('loads maps', () => {
     const data = load(3440, 1440)
-    console.log(data?.[ScreenMap.ARTIFACTS].artifact_count)
     expect(data).not.toBeNull()
   })
 })
