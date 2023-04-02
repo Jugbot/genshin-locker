e1323c0b31624c2621b90ae3cc8cfc0634981f76
Lucas Pollice <lp1935@nyu.edu>
2023-04-02 19:36:57 -0400

    qs

--- lint-staged.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  // Pass as function to process entire repo instead of only staged.
-  '*': () => [
-    'yarn format:write',
-    'yarn lint:write',
-    'yarn check-types',
-    'yarn test',
-  ],
-}
