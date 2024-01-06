e6bd469f2195aa97cfe15ac651f34b358787fb2f
Lucas Pollice <lp1935@nyu.edu>
2024-01-06 14:43:06 -0500

    init

--- packages/automation/src/scoring/const.ts
+++ packages/automation/src/scoring/const.ts
@@ -3,7 +3,8 @@ import path from 'path'
 
 export const SCRIPT_DIR_NAME = 'ArtifactScripts'
 export const SCRIPT_DIR = path.join(app.getPath('userData'), SCRIPT_DIR_NAME)
-export const RESOURCES_DIR = app.isPackaged
-  ? process.resourcesPath
-  : path.join(app.getAppPath(), 'resources')
+export const RESOURCES_DIR = path.join(
+  app.isPackaged ? process.resourcesPath : app.getAppPath(),
+  'resources'
+)
 export const RESOURCES_SCRIPT_DIR = path.join(RESOURCES_DIR, 'ArtifactScripts')
